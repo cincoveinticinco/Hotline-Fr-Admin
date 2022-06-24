9aed16dc20db19089f7aa19c2906f9ff2537a9d6
AndresGo <93600613+Josego525@users.noreply.github.com>
2022-06-24 11:08:37 -0500

    .

--- src/app/components/admin-home/add-comment-report/add-comment-report.component.ts
+++ src/app/components/admin-home/add-comment-report/add-comment-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-comment-report',
@@ -7,13 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCommentReportComponent implements OnInit {
 
+  @Output() closePanel = new EventEmitter<string>();
+
+  heightTextarea: number = 15;
+  isOpenTexarea: boolean = false;
+  report: any = null;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   close() {
-    
+    this.closePanel.emit();
+  }
+
+  toggleTextarea(open: boolean = false) {
+    this.isOpenTexarea = open ? true : !this.isOpenTexarea;
+    this.heightTextarea = this.isOpenTexarea ? 100 : 15;
   }
 
 }

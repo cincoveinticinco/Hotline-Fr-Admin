00ea20b2c5624816fe2330ce92dcb6df7f79bc41
AndresGo <93600613+Josego525@users.noreply.github.com>
2022-07-05 11:34:38 -0500

    .

--- src/app/components/admin-home/add-incident/add-incident.component.ts
+++ src/app/components/admin-home/add-incident/add-incident.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-incident',
@@ -12,14 +13,31 @@ export class AddIncidentComponent implements OnInit {
   @Output() closePanel = new EventEmitter<string>();
 
   loading: boolean = false;
+  form: FormGroup = this.fb.group({});
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.initFormFields();
   }
 
   close() {
     this.closePanel.emit();
+  }
+
+  initFormFields() {
+		this.form = this.fb.group({
+			'reporter': [null, Validators.required],
+      'send_emails': [ false, Validators.required]
+		});
+	}
+
+  onSubmit() {
+    console.log('enviado');
+    console.log(this.form);
+    console.log(this.form.get('send_emails')?.value);
   }
 
 }

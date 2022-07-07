d94bd10e381f1e54a75d151b4284601cd52a5da5
Mauricio525 <mauricio.p@cincoveinticinco.com>
2022-07-07 14:07:45 -0500

    .

--- src/app/components/admin-home/admin-home.component.ts
+++ src/app/components/admin-home/admin-home.component.ts
@@ -13,7 +13,7 @@ export class AdminHomeComponent implements OnInit {
 
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  displayedColumns = ['status', 'incidentType', 'date', 'project', 'incident', 'assign', 'comments'];
+  displayedColumns = ['number_report','status', 'incidentType', 'date', 'project', 'incident', 'assign', 'comments'];
   searchProperties: string[] = ['r_status_txt', 'Incident_type', 'p_name', 'incident_description'];
   dataSource: any[] = [];
   filteredDataSource: any[] = [];

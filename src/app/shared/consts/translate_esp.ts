2376c02ca358319e75fa9ad08dc9c677c5242a98
Yojhan Rodriguez <103947261+Mauricio525@users.noreply.github.com>
2022-07-12 17:09:25 -0500

    .

--- src/app/shared/consts/translate_esp.ts
+++ src/app/shared/consts/translate_esp.ts
@@ -1,5 +1,27 @@
 export const TRANSLATE_ESP: any = {
-  'incidents': 'Incidentes',
-  'date': 'Fecha',
-  'add incident': 'Agregar incidente'
+	'incidents': 'Incidentes',
+	'date': 'Fecha',
+	'add incident': 'Agregar incidente',
+	'all centers': 'Todos los centros',
+	'add project': 'Añadir proyecto',
+	'production Company': 'Compañia de producción',
+	'project': 'Proyecto',
+	'season': 'Temporada',
+	'center': 'Centro',
+	'location': 'Locación',
+	'users': 'Usuarios',
+	'open inc': 'Inc. abiertos',
+	'closed inc': 'Inc. cerrados',
+	'no data found': 'Datos no encontrados',
+	'merge': 'Unir',
+	'project name': 'Nombre de proyecto',
+	'abbreviation': 'Abreviación',
+	'add': 'Añadir',
+	'delete': 'Borrar',
+	'email': 'Correo',
+	'save': 'Guardar',
+	'project users': 'Usuarios del proyecto'
+
+
+
 }
